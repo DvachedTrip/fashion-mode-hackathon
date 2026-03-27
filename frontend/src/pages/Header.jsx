8412b9807342a3ee40c5aeee7ddf304e62bc31fb
Lonised <pak.promised@gmail.com>
2026-03-27 22:09:33 +0500

    0.0.4

---
+++ frontend/src/pages/Header.jsx
@@ -0,0 +1,28 @@
+import styles from '../assets/css/Header.module.css';
+
+export default function Header() {
+    return (
+        <header className={styles["header"]}>
+            <div className={styles["header-left"]}>
+                <div className={styles["logo"]}>
+                    <span style={{fontWeight: 'bold', fontSize: '24px', color: '#fff'}}>RAÚM</span>
+                </div>
+            </div>
+
+            <div className={styles["header-mid"]}>
+                <a href="/shop">SHOP</a>
+                <a href="/collections">COLLECTIONS</a>
+                <a href="/about">ABOUT</a>
+            </div>
+
+            <div className={styles["header-right"]}>
+                <div className={styles["theme-toggle"]}>
+                    [ <span className={styles["theme-active"]}>DARK</span> / LIGHT ]
+                </div>
+                <a href="/search">SEARCH</a>
+                <a href="/account">ACCOUNT</a>
+                <a href="/bag">BAG</a>
+            </div>
+        </header>
+    );
+}
